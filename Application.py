948bc70f50b05af6abf56f709096843b2e1b9bcf
BillyBananaseed <BillyBananaseed@c71ed615-e845-0410-b618-8d6d77b65957>
2008-04-18 19:17:08 +0000

    Committed my changes.

--- Application.py
+++ Application.py
@@ -6,15 +6,15 @@ import pickle
 # This class contains the methods that correspond to URLs
 class CTFServer:
 
-	preferencesArray = {}
+	preferencesArray = []
 
 	# This method (index) will be called if no other URL is given
 	# /ctf-app/
 	@cherrypy.expose
 	def index(self, *args, **kwargs):
 		
-		return "<p>Foo Bar!</p><p>%s</p>" % kwargs
-	
+		return "<p>Foo Bar!</p>"
+
 	# /ctf-app/scoreboard/
 	@cherrypy.expose	
 	def scoreboard(self, *args, **kwargs):
@@ -26,57 +26,55 @@ class CTFServer:
 	# /ctf-app/preferences/
 	@cherrypy.expose
 	def preferences(self, *args, **kwargs):
-		return "<p>Preferences stuff goes here...</p>"
+		f = open("/home/cs206/facebook-ctf/static/preferences.html")
+		html = f.read()
+		f.close()
+		uid = kwargs['fb_sig_user']
+		try:
+			prefs = pickle.load(open("/home/cs206/facebook-ctf/userprefs/%s" % uid))
+		except IOError:
+			# user hasn't saved preferences before, use default
+			prefs = {'teamName':'The Leopards','teamColor':'0'}
+			prefs['individualColor'] = prefs['grid'] = '0'
+			prefs['numberOfFlags'] = prefs['terrain'] = '0'
+		# set the default value for the forms
+		html = html.replace('userTeamName', prefs['teamName'])
+		last = 0
+		for pref in ['teamColor','individualColor','numberOfFlags','terrain','grid']:
+			s = prefs[pref]
+			start = html.find(pref, last)
+			end = html.find("</select>", start)
+			section = html[start:end]
+			section = section.replace('value="%s"' % s, 'value="%s" selected' % s)
+			html = html[:start] + section + html[end:]
+			last = end
+		return html
 		
-
 	@cherrypy.expose
 	def saveprefs(self, *args, **kwargs):
-		
-		preferenceArraySize = 0
+		#preferenceArraySize = 0
 
 		#creates a string
-		keyStrings = "teamName teamColor individualColor numberOfFlags terrain grid"
+		#keyStrings = "teamName teamColor individualColor numberOfFlags terrain grid"
 
 		#splits the string into several strings and splits by spaces
-		keyStrings.split()
+		#keyStrings.split()
 
-		for preference in keyStrings:
-			preferencesArray[preferenceArraySize] = kwargs[keyStrings]
-			preferenceArraySize += 1
+		#for preference in keyStrings:
+		#	self.preferencesArray[preferenceArraySize] = kwargs[preference]
+		#	preferenceArraySize += 1
 
-		return str(preferences)
-
-		#prefSize=0
-		#a=0
-		#y=0
-		#foundString = False
-
-		#while y < 7:
-		#	if kwargs[a] == "teamName":
-		#		preferences[prefSize] = kwargs["teamName"]
-		#		foundString = true
-		#	elif kwargs[a] == "teamColor":
-		#		preferences[prefSize] = kwargs[a]
-		#		foundString = true
-		#	elif kwargs[a] == "individualColor":
-		#		preferences[prefSize] = kwargs[a]
-		#		foundString = true
-		#	elif kwargs[a] == "numberOfFlags":
-		#		preferences[prefSize] = kwargs[a]
-		#		foundString = true
-		#	elif kwargs[a] == "terrain":
-		#		preferences[prefSize] = kwargs[a]
-		#		foundString = true
-		#	elif kwargs[a] == "grid":
-		#		preferences[prefSize] = kwargs[a]
-		#		foundString = true
-		#	
-		#	if foundString == True:
-		#		y += 1
-		#		z += 1
-		#		a += 1
-		#		foundString = False
-		#
+		#return str(preferences)
+		userprefs = {}
+		uid = kwargs['fb_sig_user']
+		userprefs['teamName'] = kwargs['teamName']
+		userprefs['teamColor'] = kwargs['teamColor']
+		userprefs['individualColor'] = kwargs['individualColor']
+		userprefs['numberOfFlags'] = kwargs['numberOfFlags']
+		userprefs['terrain'] = kwargs['terrain']
+		userprefs['grid'] = kwargs['grid']
+		pickle.dump(userprefs, open("/home/cs206/facebook-ctf/userprefs/%s" % uid, 'w'))
+		return "Preferences saved."
 
 	@cherrypy.expose
 	def game(self, *args, **kwargs):
